d20b63dc0ddf783f6d5720ef1941f75592bc1da9
rascheiw <122454673+rascheiw@users.noreply.github.com>
2024-02-02 21:03:04 +0100

    ajout affichage jeux de données utilisées quand on fait des prédictions

--- reports/demo_streamlit.py
+++ reports/demo_streamlit.py
@@ -3,6 +3,7 @@ import subprocess
 import requests
 import config
 import time
+import pandas as pd
 
 def progress_bar():
         progress_text = "En attente de la réponse de l'API"
@@ -208,11 +209,101 @@ if st.sidebar.button("Vérifier l'API"):
 # Bouton pour effectuer une prédiction classe indemnes via une requete sur l'API
 if st.sidebar.button("Effectuer une prédiction indemnes"):
     result_container = st.empty()
+    indemnes = {
+    "place": [1],
+    "catu": [1],
+    "sexe": [2],
+    "trajet": [3],
+    "locp": [0],
+    "actp": [0],
+    "etatp": [0],
+    "cat_age": [6],
+    "secu0": [0],
+    "secu1": [1],
+    "secu2": [0],
+    "secu3": [0],
+    "secu4": [0],
+    "secu5": [0],
+    "secu6": [0],
+    "secu7": [0],
+    "secu8": [0],
+    "secu9": [0],
+    "catv": [7],
+    "obs": [0],
+    "obsm": [0],
+    "choc": [4],
+    "manv": [2],
+    "an": [8],
+    "mois": [4],
+    "lum": [1],
+    "agg": [2],
+    "int": [1],
+    "atm": [3],
+    "col": [2],
+    "weekday": [1],
+    "hr": [11],
+    "catr": [2],
+    "circ": [1],
+    "vosp": [0],
+    "prof": [1],
+    "plan": [1],
+    "surf": [3],
+    "infra": [0],
+    "situ": [1]
+}
+    df = pd.DataFrame(indemnes)
+    df.rename(index={0: "Jeux de données utilisés"}, inplace=True)
+    st.dataframe(df)
     progress_bar()
     make_prediction_Indemnes()
 
 # Bouton pour effectuer une prédiction classe bléssés et tués via une requete sur l'API
 if st.sidebar.button("Effectuer une prédiction victimes"):
     result_container = st.empty()
+    victimes = {
+    "place": [1],
+    "catu": [1],
+    "sexe": [1],
+    "trajet": [0],
+    "locp": [0],
+    "actp": [0],
+    "etatp": [0],
+    "cat_age": [3],
+    "secu0": [0],
+    "secu1": [1],
+    "secu2": [0],
+    "secu3": [0],
+    "secu4": [0],
+    "secu5": [0],
+    "secu6": [0],
+    "secu7": [0],
+    "secu8": [0],
+    "secu9": [0],
+    "catv": [7],
+    "obs": [0],
+    "obsm": [1],
+    "choc": [1],
+    "manv": [1],
+    "an": [9],
+    "mois": [12],
+    "lum": [5],
+    "agg": [2],
+    "int": [2],
+    "atm": [1],
+    "col": [6],
+    "weekday": [6],
+    "hr": [17],
+    "catr": [4],
+    "circ": [2],
+    "vosp": [3],
+    "prof": [1],
+    "plan": [1],
+    "surf": [1],
+    "infra": [0],
+    "situ": [1]
+}
+    df = pd.DataFrame(victimes)
+    df.rename(index={0: "Jeux de données utilisés"}, inplace=True)
+    st.dataframe(df)
     progress_bar()
     make_prediction_Victimes()
