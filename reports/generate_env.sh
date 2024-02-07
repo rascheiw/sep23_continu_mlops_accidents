8d7de227219337a383e07772b3fccc6850946eae
rascheiw <122454673+rascheiw@users.noreply.github.com>
2024-02-07 01:06:12 +0100

    modif generate_env.sh

--- reports/generate_env.sh
+++ reports/generate_env.sh
@@ -4,12 +4,6 @@
 username_env=$1
 password_env=$2
 
-# Vérifier si les variables sont définies
-if [ -z "$username_env" ] || [ -z "$password_env" ]; then
-  echo "Erreur: les variables ne sont pas définies."
-  exit 1
-fi
-
 # Générer le fichier .env
 echo "STREAMLIT_USERNAME=$username_env" > .env
 echo "STREAMLIT_PASSWORD=$password_env" >> .env
