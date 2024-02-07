092ea79b774f572aed4605d1f270e95a7c948fa2
rascheiw <122454673+rascheiw@users.noreply.github.com>
2024-02-07 01:02:00 +0100

    modif build.yml Dockerfile generate_env.sh

--- reports/generate_env.sh
+++ reports/generate_env.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Récupérer les secrets GitHub
+# Récupérer les arguments
 username_env=$1
 password_env=$2
+
+# Vérifier si les variables sont définies
+if [ -z "$username_env" ] || [ -z "$password_env" ]; then
+  echo "Erreur: les variables ne sont pas définies."
+  exit 1
+fi
 
 # Générer le fichier .env
 echo "STREAMLIT_USERNAME=$username_env" > .env
